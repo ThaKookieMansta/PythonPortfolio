5c15369313bb60373cb4d31dabdc3f5f8bbd858f
Chris Mukirae <chriskagiko@gmail.com>
2023-03-19 10:23:44 +0300

    First commit
    This is the first commit for the wifi cafe project.

---
+++ wifi-cafe/README.md
@@ -0,0 +1,11 @@
+# Wifi Cafe
+
+This web application shows you the various cafe's in London that are remote work friendly. Use it to know where to go 
+for coffee and work.
+
+## Installation
+
+To use this, you need to install Flask on your system.
+
+## License
+This web app is free to use.
--- wifi-cafe/server.py
+++ wifi-cafe/server.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
-
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
+db.init_app(app)
+
+
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    map_url = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=False)
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def homepage():
+    all_cafes = db.session.query(Cafe).all()
+    today = datetime.date.today()
+    year = today.year
+    return render_template("index.html", year=year, cafe=all_cafes)
 
 
 if __name__ == "__main__":
